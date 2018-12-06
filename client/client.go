0298eb765b3b717fc4ac4de48fca33aa8b914cae
Axel Gonzalez <loox@e-shell.net>
2018-12-06 01:29:39 -0600

    Move xhttp to common

--- client/client.go
+++ client/client.go
@@ -3,68 +3,11 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"strings"
-	"io/ioutil"
-	"net/http"
 
 	"../common"
 	//"time"
 )
-
-
-const HOST string = "http://localhost"
-
-
-func xhttp(m string, p int, u string, b string) (string, error) {
-	var cl *http.Client
-	var req *http.Request
-	var res *http.Response
-	var r []byte
-	var rd io.Reader
-	var err error
-
-	var ret string
-
-
-	cl = &http.Client{}
-
-	url := fmt.Sprintf("%s:%d%s", HOST, common.I_PORT, u)
-
-	if (b == "") {
-		req, err = http.NewRequest(m, url, nil)
-	} else {
-		rd = strings.NewReader(b)
-		req, err = http.NewRequest(m, url, rd)
-		if (err != nil) {
-			fmt.Fprintf(os.Stderr, "xhttp: %v\n\n", err)
-			return ret, err
-		}
-	}
-
-
-	req.Header.Add("Connection", "close")
-	req.TransferEncoding = []string{"text/json"}
-
-	res, err = cl.Do(req)
-	if (err != nil) {
-		fmt.Fprintf(os.Stderr, "xhttp: %v\n\n", err)
-		return ret, err
-	}
-	defer res.Body.Close()
-
-
-	r, err = ioutil.ReadAll(res.Body)
-	if (err != nil) {
-		fmt.Fprintf(os.Stderr, "xhttp: %v\n\n", err)
-		return ret, err
-	}
-
-	ret = string(r)
-
-	return ret, err
-}
 
 
 func analysis() {
@@ -84,12 +27,12 @@ func invoicing() {
 
 
 	fmt.Printf("Testing root\n")
-	r, _ = xhttp("GET", common.I_PORT, "/", "")
+	r, _ = common.Xhttp("GET", common.I_PORT, "/", "")
 	fmt.Printf("%s\n", r)
 	fmt.Printf("================\n")
 
 	fmt.Printf("Testing root POST\n")
-	r, _ = xhttp("POST", common.I_PORT, "/", "qwerty")
+	r, _ = common.Xhttp("POST", common.I_PORT, "/", "qwerty")
 	fmt.Printf("%s\n", r)
 	fmt.Printf("================\n")
 
--- common/common.go
+++ common/common.go
@@ -2,8 +2,20 @@
 package common
 
 import (
-	//"time"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"io/ioutil"
+	"net/http"
 )
+
+
+const HOST string = "http://localhost"
+
+const A_PORT = 12001
+const I_PORT = 12002
+const R_PORT = 2003
 
 
 type Invoice struct {
@@ -16,6 +28,52 @@ type Invoice struct {
 	Amount float64	`json:"amount,omitempty"`
 }
 
-const A_PORT = 12001
-const I_PORT = 12002
-const R_PORT = 2003
+
+func Xhttp(m string, p int, u string, b string) (string, error) {
+	var cl *http.Client
+	var req *http.Request
+	var res *http.Response
+	var r []byte
+	var rd io.Reader
+	var err error
+
+	var ret string
+
+
+	cl = &http.Client{}
+
+	url := fmt.Sprintf("%s:%d%s", HOST, I_PORT, u)
+
+	if (b == "") {
+		req, err = http.NewRequest(m, url, nil)
+	} else {
+		rd = strings.NewReader(b)
+		req, err = http.NewRequest(m, url, rd)
+		if (err != nil) {
+			fmt.Fprintf(os.Stderr, "xhttp: %v\n\n", err)
+			return ret, err
+		}
+	}
+
+
+	req.Header.Add("Connection", "close")
+	req.TransferEncoding = []string{"text/json"}
+
+	res, err = cl.Do(req)
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "xhttp: %v\n\n", err)
+		return ret, err
+	}
+	defer res.Body.Close()
+
+
+	r, err = ioutil.ReadAll(res.Body)
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "xhttp: %v\n\n", err)
+		return ret, err
+	}
+
+	ret = string(r)
+
+	return ret, err
+}
