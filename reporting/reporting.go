ebcf7b353425bc69f6730ec533c5fcb1ff1d77d4
Axel Gonzalez <loox@e-shell.net>
2018-12-07 00:42:46 -0600

    Add remaining controllers bodies fror reporting

--- reporting/reporting.go
+++ reporting/reporting.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-	//"strconv"
-	//"time"
+	"strconv"
+	"time"
 	"encoding/json"
-	//"math/rand"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -71,14 +70,199 @@ func all(w http.ResponseWriter, r *http.Request) {
 
 
 func client(w http.ResponseWriter, r *http.Request) {
+	var client int
+	var res string
+	var tot int
+	var err error
+	var ja map[string]interface{}
+	var jr []interface{}
+
+
+	fmt.Printf("req: %s\n", r.URL)
+
+	w.Header().Set("Content-Type", "text/json")
+
+	vars := mux.Vars(r)
+
+	client, _ = strconv.Atoi(vars["client"])
+
+	if (client == 0) {
+		w.Write([]byte("{\"status\": 200, \"msg\": \"id not found\"}"))
+		return
+	}
+
+
+	res, _ = common.Xhttp("GET", common.I_PORT, "/all", "")
+	//fmt.Printf("res: %+v\n", res)
+
+	err = json.Unmarshal([]byte(res), &ja)
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "panic: bad json from micro [%s]\n", err.Error())
+
+		w.WriteHeader(500)
+		w.Write([]byte("{\"status\": 500, \"msg\": \"Inernal server error\"}"))
+
+		return
+	}
+	if (int(ja["status"].(float64)) != 200) {
+		fmt.Fprintf(os.Stderr, "panic: micro error %d [%s]\n", int(ja["status"].(float64)), ja["msg"])
+
+		w.WriteHeader(500)
+		w.Write([]byte("{\"status\": 500, \"msg\": \"Inernal server error\"}"))
+
+		return
+	}
+	//fmt.Printf("%+v\n", ja)
+
+	tot = 0
+
+	for _, v := range(ja["results"].([]interface{})) {
+
+		if (int(v.(map[string]interface{})["client"].(float64)) != client) {
+			continue
+		}
+
+		jr = append(jr, v)
+		tot += 1
+
+	}
+	//fmt.Printf("%v\n", jr)
+
+	jm, err := json.Marshal(jr)
+
+	b := fmt.Sprintf("{\"status\": 200, \"msg\": \"ok\", \"total\": %d, \"results\": %s}", tot, jm)
+	w.Write([]byte(b))
+
 }
 
 
 func last(w http.ResponseWriter, r *http.Request) {
+	var sec int
+	var res string
+	var tot int
+	var err error
+	var ja map[string]interface{}
+	var jr []interface{}
+
+
+	fmt.Printf("req: %s\n", r.URL)
+
+	w.Header().Set("Content-Type", "text/json")
+
+	vars := mux.Vars(r)
+
+	sec, _ = strconv.Atoi(vars["sec"])
+
+	if (sec <= 0) {
+		w.Write([]byte("{\"status\": 400, \"msg\": \"bad request\"}"))
+		return
+	}
+
+
+	res, _ = common.Xhttp("GET", common.I_PORT, "/all", "")
+	//fmt.Printf("res: %+v\n", res)
+
+	err = json.Unmarshal([]byte(res), &ja)
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "panic: bad json from micro [%s]\n", err.Error())
+
+		w.WriteHeader(500)
+		w.Write([]byte("{\"status\": 500, \"msg\": \"Inernal server error\"}"))
+
+		return
+	}
+	if (int(ja["status"].(float64)) != 200) {
+		fmt.Fprintf(os.Stderr, "panic: micro error %d [%s]\n", int(ja["status"].(float64)), ja["msg"])
+
+		w.WriteHeader(500)
+		w.Write([]byte("{\"status\": 500, \"msg\": \"Inernal server error\"}"))
+
+		return
+	}
+	//fmt.Printf("%+v\n", ja)
+
+	tot = 0
+	ti := int(time.Now().Unix()) - sec
+
+	for _, v := range(ja["results"].([]interface{})) {
+
+		if (ti > int(v.(map[string]interface{})["utime"].(float64))) {
+			continue
+		}
+
+		jr = append(jr, v)
+		tot += 1
+
+	}
+	//fmt.Printf("%v\n", jr)
+
+	jm, err := json.Marshal(jr)
+
+	b := fmt.Sprintf("{\"status\": 200, \"msg\": \"ok\", \"total\": %d, \"results\": %s}", tot, jm)
+	w.Write([]byte(b))
+
 }
 
 
 func today(w http.ResponseWriter, r *http.Request) {
+	var res string
+	var tot int
+	var err error
+	var ja map[string]interface{}
+	var jr []interface{}
+
+
+	fmt.Printf("req: %s\n", r.URL)
+
+	w.Header().Set("Content-Type", "text/json")
+
+
+	res, _ = common.Xhttp("GET", common.I_PORT, "/all", "")
+	//fmt.Printf("res: %+v\n", res)
+
+	err = json.Unmarshal([]byte(res), &ja)
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "panic: bad json from micro [%s]\n", err.Error())
+
+		w.WriteHeader(500)
+		w.Write([]byte("{\"status\": 500, \"msg\": \"Inernal server error\"}"))
+
+		return
+	}
+	if (int(ja["status"].(float64)) != 200) {
+		fmt.Fprintf(os.Stderr, "panic: micro error %d [%s]\n", int(ja["status"].(float64)), ja["msg"])
+
+		w.WriteHeader(500)
+		w.Write([]byte("{\"status\": 500, \"msg\": \"Inernal server error\"}"))
+
+		return
+	}
+	//fmt.Printf("%+v\n", ja)
+
+	tot = 0
+
+	now := time.Now()
+	ty, tm, td := now.Date()
+	ti := time.Date(ty, tm, td, 0, 0, 0, 0, time.Local).Unix()
+	fmt.Printf("ti %d\n", ti)
+
+	for _, v := range(ja["results"].([]interface{})) {
+
+		if (ti > int64(v.(map[string]interface{})["utime"].(float64))) {
+			continue
+		}
+
+		jr = append(jr, v)
+		tot += 1
+
+	}
+	//fmt.Printf("%v\n", jr)
+
+	jm, err := json.Marshal(jr)
+
+	b := fmt.Sprintf("{\"status\": 200, \"msg\": \"ok\", \"total\": %d, \"results\": %s}", tot, jm)
+	w.Write([]byte(b))
+
 }
 
 
